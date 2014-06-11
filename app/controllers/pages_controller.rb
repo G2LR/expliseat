f8e613bc62e37a01a687861a97b830f9fd28d921
G2LR <g.larochebrochard@gmail.com>
2014-06-11 23:24:31 +0200

    Adding the navigation links

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,18 @@
 class PagesController < ApplicationController
+
   def home
   end
+
+  def seat
+  end
+
+  def company
+  end
+
+  def press
+  end
+
+  def contact
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get 'pages/home'
+  root 'pages#home'
+  get 'seat' => 'pages#seat'
+  get 'company' => 'pages#company'
+  get 'press' => 'pages#press'
+  get 'contact' => 'pages#contact'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
